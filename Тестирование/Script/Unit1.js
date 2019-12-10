c9d9dcacde6754fc662111714af4607b23b08b7a
Raven124 <you@example.com>
2019-12-10 16:14:20 +0700

    Разработка IT-проекта

---
+++ Тестирование/Script/Unit1.js
@@ -0,0 +1,17 @@
+﻿function Test1()
+{
+  Mobile.SetCurrent("FRD-L09");
+  ImageRepository.therapist.ImageButton_UnnamedCtrl.Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("connection").Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Нет", spBottomMost).Touch();
+  ImageRepository.therapist.TextView_10531077102.Touch();
+  ImageRepository.therapist.TextView_10531077101.Touch();
+  ImageRepository.therapist.TextView_10531077103.Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Нет", spBottomMost).Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Да", spBottomMost).Touch();
+}
+
+function Test2()
+{
+  Mobile.SetCurrent("FRD-L09");
+}
---
+++ Тестирование/Script/Unit2.js
@@ -0,0 +1,9 @@
+﻿function Test2()
+{
+  Mobile.SetCurrent("FRD-L09");
+  OCR.Recognize(Mobile.Device()).BlockByText("connection").Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Нет").Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Нет )").Touch();
+  ImageRepository.therapist.TextView_10531077101.Touch();
+  OCR.Recognize(Mobile.Device()).BlockByText("Да", spBottomMost).Touch();
+}
